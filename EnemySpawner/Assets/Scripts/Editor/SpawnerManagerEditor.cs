767341dc13ad1ff12ea753d55eac2b215fcc420a
Boldea Marius <yukki.github@gmail.com>
2023-05-23 15:31:27 +0200

    .

--- EnemySpawner/Assets/Scripts/Editor/SpawnerManagerEditor.cs
+++ EnemySpawner/Assets/Scripts/Editor/SpawnerManagerEditor.cs
@@ -2,19 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.Linq;
 
 [CustomEditor(typeof(SpawnerManager))]
 public class SpawnerManagerEditor : Editor
 {
-    // Start is called before the first frame update
-    void Start()
+	private SpawnerManager spawnerManager;
+
+    private void OnEnable()
     {
+		spawnerManager = (SpawnerManager)target;
+	}
+    void OnSceneGUI()
+    {
+        Handles.color = Color.yellow;
         
+        //Handles.DrawWireCube(spawnerManager.transform.position, spawnerManager.size);
+		ShowAndMovePoints();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+		if (GUILayout.Button("Add a cube spawner"))
+		{
+			//spawnerManager.areaHandle.Add(new Vector3());
+			spawnerManager.spawners.Add(SpawnerManager.TypeOfSpawner.Cubic, Vector3.zero);
+			spawnerManager.areaSize.Add(new Vector3(1, 1, 1));
+			// Button click event handler
+			//Debug.Log("Button clicked!");
+		}
+	}
+
+
+	void ShowAndMovePoints()
+	{
+		for (int i = 0; i < spawnerManager.spawners.Count; i++)
+		{
+			Vector3 currentPoint = spawnerManager.GetPoint(i);
+
+			if (spawnerManager.spawners.ElementAt(i).Key == SpawnerManager.TypeOfSpawner.Cubic)
+			{
+				Handles.DrawWireCube(currentPoint, spawnerManager.areaSize[i]);
+			}
+			EditorGUI.BeginChangeCheck();
+			Vector3 newPosition = Handles.PositionHandle(currentPoint, Quaternion.identity);
+			if (EditorGUI.EndChangeCheck())
+			{
+				Undo.RecordObject(spawnerManager, "Moved spawner");
+				EditorUtility.SetDirty(spawnerManager);
+				spawnerManager.spawners[spawnerManager.spawners.ElementAt(i).Key] = newPosition;
+			}
+		}
+	}
+
 }
--- EnemySpawner/Assets/Scripts/SpawnerManager.cs
+++ EnemySpawner/Assets/Scripts/SpawnerManager.cs
@@ -1,9 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class SpawnerManager : MonoBehaviour
 {
+    //public Vector3 size = new Vector3(1,1,1);
+
+    public List<Vector3> areaSize = new List<Vector3>();
+
+    public enum TypeOfSpawner
+    {
+        Cubic,
+        Spherical,
+        Custom
+    }
+    public Dictionary<TypeOfSpawner, Vector3> spawners = new Dictionary<TypeOfSpawner, Vector3>();
+
+    public Vector3 GetPoint(int pointIndex)
+    {
+        if (pointIndex < 0 || pointIndex >= spawners.Count)
+        {
+            Debug.Log("SpawnerManager.cs: WARNING: pointIndex out of range: " + pointIndex + " curve length: " + spawners.Count);
+            return Vector3.zero;
+        }
+        return transform.TransformPoint(spawners.ElementAt(pointIndex).Value);
+    }
     // Start is called before the first frame update
     void Start()
     {
